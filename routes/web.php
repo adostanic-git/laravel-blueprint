32f7fc42ab6feb42585f4c61c2e61e83aeffee79
adostanic-git <andrejdostanic9@gmail.com>
2026-01-05 22:51:21 +0100

    Napravljen yaml fajl i pokrenut blueprint build. Dodati seederi

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/prodavnica', [ProizvodController::class, 'prodavnica']);
-Route::post('/naruci', [NarudzbinaController::class, 'kreirajNarudzbinu']);
-Route::get('/moje-narudzbine', [NarudzbinaController::class, 'mojeNarudzbine']);
 
